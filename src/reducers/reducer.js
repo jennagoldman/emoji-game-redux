74fe91ed9587e90fc4d6ff03a9e6ddf6ff725938
Jenna Goldman <jenna.goldman89@gmail.com>
2020-05-12 16:07:26 -0700

    fix studies state typo in reducer

--- src/reducers/reducer.js
+++ src/reducers/reducer.js
@@ -10,11 +10,11 @@ export default function reducer(state = initialState, action) {
     case 'DRINK_COFFEE':
       return {...state, coffees: state.coffees + 1};
     case 'EAT_SNACK':
-      return {...state, snacks: state.snacks + 1 };
+      return {...state, snacks: state.snacks + 1};
     case 'TAKE_NAP':
-      return {...state, naps: state.naps + 1 };
+      return {...state, naps: state.naps + 1};
     case 'STUDY':
-      return {...state, study: state.study + 1 };
+      return {...state, studies: state.studies + 1};
     default:
       return console.log(`unhandled type: ${action.type}`);
   }
