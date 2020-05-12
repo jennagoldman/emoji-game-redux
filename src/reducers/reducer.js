a2827caa15e03691db68440bb363e1bdce3315d5
Jenna Goldman <jenna.goldman89@gmail.com>
2020-05-12 15:46:17 -0700

    move handleSelection functionality to reducer

--- src/reducers/reducer.js
+++ src/reducers/reducer.js
@@ -1,6 +1,18 @@
 export default function reducer(state, action) {
   switch(action.type) {
+    case 'DRINK_COFFEE':
+      this.setState(state => ({ coffees: state.coffees + 1 }));
+      break;
+    case 'EAT_SNACK':
+      this.setState(state => ({ snacks: state.snacks + 1 }));
+      break;
+    case 'TAKE_NAP':
+      this.setState(state => ({ naps: state.naps + 1 }));
+      break;
+    case 'STUDY':
+      this.setState(state => ({ studies: state.studies + 1 }));
+      break;
     default:
-      return state;
+      console.log(`unhandled type: ${action.type}`);
   }
 }
