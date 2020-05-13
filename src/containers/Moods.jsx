ee9e7c43e103224312c708b8476a41ba9700c880
Jenna Goldman <jenna.goldman89@gmail.com>
2020-05-12 17:52:45 -0700

    bring in useSelector to get face to render in Face component

--- src/containers/Moods.jsx
+++ src/containers/Moods.jsx
@@ -1,14 +1,13 @@
 import React, { Component } from 'react';
 import Controls from '../components/Controls/Controls.jsx';
 import Face from '../components/Face/Face.jsx';
-import { useSelector } from 'react-redux';
 
 export default class Moods extends Component {
 
   render() {
     const { coffees, snacks, naps, studies } = this.state;
-    const face = useSelector(getFace);
-
+    const face = getFace(this.state);
+    
     return (
       <>
         <Controls>
