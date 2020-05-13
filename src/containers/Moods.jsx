c653724a3d70b48b8b946571999b2adfb9d1a3f1
Jenna Goldman <jenna.goldman89@gmail.com>
2020-05-12 17:12:35 -0700

    move isTired, isHyper, isEducated, isHungry, and getFace functions from Mood to moodSelectors

--- src/containers/Moods.jsx
+++ src/containers/Moods.jsx
@@ -2,24 +2,8 @@ import React, { Component } from 'react';
 import Controls from '../components/Controls/Controls.jsx';
 import Face from '../components/Face/Face.jsx';
 
-export const isTired = state => state.coffees < 1 && state.naps < 1;
-export const isHyper = state => state.coffees > 3;
-export const isEducated = state => state.studies > 2;
-export const isHungry = state => state.snacks < 1;
-
-export const getFace = state => {
-  if(isTired(state) && isHungry(state)) return '🤬';
-  if(isHyper(state) && isHungry(state)) return '🤮';
-  if(isTired(state)) return '😴';
-  if(isHyper(state)) return '🙀';
-  if(isEducated(state)) return '🤯';
-  if(isHungry(state)) return '😡';
-
-  return '😀';
-};
-
 export default class Moods extends Component {
-  
+
   render() {
     const { coffees, snacks, naps, studies } = this.state;
     const face = getFace(this.state);
