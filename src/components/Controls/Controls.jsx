afa289927cdd598263ba2af6af7e27fa1d964487
Jenna Goldman <jenna.goldman89@gmail.com>
2020-05-12 19:20:05 -0700

    undo last change

--- src/components/Controls/Controls.jsx
+++ src/components/Controls/Controls.jsx
@@ -1,24 +1,15 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import styles from './Controls.css';
-import { useSelector, useDispatch } from 'react-redux';
-import { drinkCoffee, eatSnack, takeNap, study } from '../../actions/moodActions.js';
 
-const Controls = () => {
-  const { coffees, snacks, naps, studies } = useSelector(state => state);
-  const dispatch = useDispatch();
-  
-  const handleSelection = action => {
-    dispatch(action);
-  }
+const Controls = ({ children }) => (
+  <section className={styles.Controls}>
+    {children}
+  </section>
+);
 
-  return (
-    <section className={styles.Controls}>
-      <button onClick={() => handleSelection(drinkCoffee())}>coffee - {coffees}</button>
-      <button onClick={() => handleSelection(eatSnack())}>snacks - {snacks}</button>
-      <button onClick={() => handleSelection(takeNap())}>naps - {naps}</button>
-      <button onClick={() => handleSelection(study())}>studies - {studies}</button>
-    </section>
-  );
+Controls.propTypes = {
+  children: PropTypes.node
 };
 
 export default Controls;
--- src/containers/Moods.jsx
+++ src/containers/Moods.jsx
@@ -1,13 +1,25 @@
 import React from 'react';
 import Controls from '../components/Controls/Controls.jsx';
 import Face from '../components/Face/Face.jsx';
+import { useSelector, useDispatch } from 'react-redux';
+import { drinkCoffee, eatSnack, takeNap, study } from '../actions/moodActions.js';
 
 const Moods = () => {
-
+  const { coffees, snacks, naps, studies } = useSelector(state => state);
+  const dispatch = useDispatch();
+  
+  const handleSelection = action => {
+    dispatch(action);
+  }
 
   return (
       <>
-        <Controls />
+        <Controls>
+          <button onClick={() => handleSelection(drinkCoffee())}>coffee - {coffees}</button>
+          <button onClick={() => handleSelection(eatSnack())}>snacks - {snacks}</button>
+          <button onClick={() => handleSelection(takeNap())}>naps - {naps}</button>
+          <button onClick={() => handleSelection(study())}>studies - {studies}</button>
+        </Controls>
         <Face />
       </>
     );
