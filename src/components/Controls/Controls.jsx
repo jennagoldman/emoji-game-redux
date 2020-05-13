6ed4163a6a081282af22dd82b9aa1f57ca20df28
Jenna Goldman <jenna.goldman89@gmail.com>
2020-05-12 19:10:01 -0700

    move control buttons and associated imports and callbacks to Controls component

--- src/components/Controls/Controls.jsx
+++ src/components/Controls/Controls.jsx
@@ -1,15 +1,24 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import styles from './Controls.css';
+import { useSelector, useDispatch } from 'react-redux';
+import { drinkCoffee, eatSnack, takeNap, study } from '../../actions/moodActions.js';
 
-const Controls = ({ children }) => (
-  <section className={styles.Controls}>
-    {children}
-  </section>
-);
+const Controls = () => {
+  const { coffees, snacks, naps, studies } = useSelector(state => state);
+  const dispatch = useDispatch();
+  
+  const handleSelection = action => {
+    dispatch(action);
+  }
 
-Controls.propTypes = {
-  children: PropTypes.node
+  return (
+    <section className={styles.Controls}>
+      <button onClick={() => handleSelection(drinkCoffee())}>coffee - {coffees}</button>
+      <button onClick={() => handleSelection(eatSnack())}>snacks - {snacks}</button>
+      <button onClick={() => handleSelection(takeNap())}>naps - {naps}</button>
+      <button onClick={() => handleSelection(study())}>studies - {studies}</button>
+    </section>
+  );
 };
 
 export default Controls;
